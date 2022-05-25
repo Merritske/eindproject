2b87ba0da55bdab8d78a3cdfb8da595d2de08bbd
merritske <merritske@gmail.com>
2022-05-25 21:01:26 +0200

    listgroup

--- src/components/Reizen.jsx
+++ src/components/Reizen.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useContext, useEffect, useState } from 'react';
-import { Col, Container, Row, Button } from 'react-bootstrap';
+import { Col, Container, Row, Button, ListGroup, ListGroupItem } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 
 import { UserProvider } from '../context';
@@ -24,22 +24,22 @@ function Reizen() {
     const [datumId, setDatumId] = useState()
 
     useEffect(() => {
-           
+
         for (let i = 0; i < content.length; i++) {
-             console.log(trip)
+            console.log(trip)
             if (content[i].title === trip) {
                 setReis(content[i])
             }
         }
         console.log(reis)
     })
-//soms begint die data te laden en soms niet, als je []depency arry er zet stopt die maar dan verschijnt de data niet op het scherm
-//WAAROM WERKT DIT DE ENE KEER WEL EN DE ANDERE KEER NIET?????????
+    //soms begint die data te laden en soms niet, als je []depency arry er zet stopt die maar dan verschijnt de data niet op het scherm
+    //WAAROM WERKT DIT DE ENE KEER WEL EN DE ANDERE KEER NIET?????????
 
-//reizen fetchen
+    //reizen fetchen
     function handleFetch() {
         console.log("klik")
-        fetch("http://localhost:5001/reizen")
+        fetch("/reizen")
             .then(res => res.json())
             .then(trip => {
                 for (let y = 0; y < trip.length; y++) {
@@ -50,112 +50,108 @@ function Reizen() {
                 }
 
 
-            }) 
-         
+            })
+
     }
 
-//console.log(geplandR)
+console.log(geplandR)
 
-// //opzoeken om reizen te updaten bij inschrijven + lijst van de deelnemer van geplande reizen aanpassen
-// //dus PUT "/reizen"
-// // en PUT "/user" PROFIEL VAN DE USER NOG MAKEN => hier ook wachtwoord en username veranderen en gegevens aanpassen, uitschrijven nieuwsbrief, uitschrijven website => protectedROUTE!!! 
- function handleInschrijven (e){ 
-     const deelnemer = user.username
-    setDeelnemers(
-     [...geplandR[e.target.id].deelnemers, deelnemer]
-      )
-    
+    // //opzoeken om reizen te updaten bij inschrijven + lijst van de deelnemer van geplande reizen aanpassen
+    // //dus PUT "/reizen"
+    // // en PUT "/user" PROFIEL VAN DE USER NOG MAKEN => hier ook wachtwoord en username veranderen en gegevens aanpassen, uitschrijven nieuwsbrief, uitschrijven website => protectedROUTE!!! 
+    function handleInschrijven(e) {
+        
+        const deelnemer = user.username
+        setDeelnemers(
+            [...geplandR[e.target.id].deelnemers, deelnemer]
+        )
+
         setDatumId(geplandR[e.target.id]._id)
 
-     console.log(e.target.id)
-    console.log(deelnemers)
-const requestOptions = {
-        method: 'PUT',
-     headers:{
-         'Content-Type': 'application/json',
-         "Content-Length": '1500'
-    },
-     body: JSON.stringify( 
-    
-     {
-         _id: datumId,
-        deelnemers: deelnemers}
-  
-         )
- };
- console.log("going on a trip?!")
-//PAS NA DE TWEEDE KEER KLIKKEN UPDATE DIE DE GEGEVENS??????
-//CONSOLE.LOG in backend laat de geupdate gegevens zien, zowel via insomnia als de gegevens via html ingegeven 
-//in insomnia geeft die melding van "cannot PUT"
-//in MongoDB gebeurt niets
- console.log(requestOptions.body);
+        console.log(e.target.id)
+        console.log(deelnemers)
 
- fetch(`http://localhost:5001/reizen/${trip}`
- ,requestOptions
- )
- .then(res=>  res.json())
- .then((data)=>{
-  
- console.log(data)
- }
-      
+        console.log("going on a trip?!")
+        //PAS NA DE TWEEDE KEER KLIKKEN UPDATE DIE DE GEGEVENS??????
+        //CONSOLE.LOG in backend laat de geupdate gegevens zien, zowel via insomnia als de gegevens via html ingegeven 
+        //in insomnia geeft die melding van "cannot PUT"
+        //in MongoDB gebeurt niets
 
- )
 
-  }
+        fetch(`/reizen/${trip}` , 
+        {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                    "Content-Length": '1500'
+                },
+                body: JSON.stringify(
+                    {
+                        _id: datumId,
+                        deelnemers: deelnemers
+                    }
+                )
+            }
+        )
+            .then(res => res.json())
+            .then((data) => {
+                console.log(data)
+            }
+            )
+    }
 
- 
+
     return (
         <Container className='mt-5'>
 
 
 
             <Row className='mt-5'  >
-
+      <Col md={8} >
                 <h1 className='mt-5'>{reis.title}</h1>
-                <Col>
+          
                     <p>{reis.tekst}</p>
                 </Col>
-                <Col>
-                    <h3> beschikbare datums om in te schrijven</h3> 
+                <Col md={4} className=' mt-5'>
+                    <h3> beschikbare datums om in te schrijven</h3>
                     <button onClick={handleFetch}>Bekijk de beschikbare datums</button>
- {
-    geplandR.map((date, index)=>(
+                    {
+                        geplandR.map((date, index) => (
 
-        <ul key={index}>
-        <li>
-    
-{
-    new Date(date.datum).toLocaleDateString("nl-BE", { weekday: "long", year: "numeric", month: "long", day: "numeric"})
-}
-           {
-               date.deelnemers.map((deeln, ind)=>(
+                            <ListGroup key={index}>
+                                <ListGroupItem>
 
-     <ul key={ind} >
-               <li>
-                   {deeln}
-               </li>
-           
-           
-           </ul>
+                                    {
+                                        new Date(date.datum).toLocaleDateString("nl-BE", { weekday: "long", year: "numeric", month: "long", day: "numeric" })
+                                    }
+                                    {
+                                        date.deelnemers.map((deeln, ind) => (
 
-               ))
-           }
-      
+                                            <ListGroup key={ind} className="w-25 mx-auto">
+                                                <ListGroupItem className='m-1'>
+                                                    {deeln}
+                                                </ListGroupItem>
 
-       </li> 
-       <Button id={index} className='m-3' onClick={handleInschrijven}>Inschrijven</Button>
 
-</ul>
-    ))
-} 
-                
+                                            </ListGroup>
 
-           
+                                        ))
+                                    }
+
+
+                                </ListGroupItem>
+                                <Button id={index} className='m-3' onClick={handleInschrijven}>Inschrijven</Button>
+
+                            </ListGroup>
+                        ))
+                    }
+
+
+
                 </Col>
 
 
-               
+
             </Row>
 
 
